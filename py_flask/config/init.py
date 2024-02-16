947077b4d806a82bf3862e1ac6e099ca17297858
exoriparian <jonah777@gmail.com>
2024-02-16 07:07:32 +0000

    update py_flask/config/requirements_prod.txt (rename) with new versions, remove old imports

--- py_flask/config/init.py
+++ py_flask/config/init.py
@@ -2,20 +2,13 @@
 """The app module, containing the app factory function."""
 import logging
 import sys
-from datetime import datetime
-
-from flask import Flask, render_template
-
+from flask import Flask
 from py_flask.routes.public_routes import blueprint_edurange3_public
 from py_flask.routes.student_routes import blueprint_edurange3_student
 from py_flask.routes.instructor_routes import blueprint_edurange3_instructor
 from py_flask.routes.scenario_routes import blueprint_edurange3_scenarios
-
-
 from py_flask.utils import commands
 from py_flask import database
-# from py_scripts import commands, public, user, tutorials, api
-
 from py_flask.config.extensions import (
     bcrypt,
     cache,
@@ -37,11 +30,9 @@ def create_app(config_object="py_flask.config.settings"):
     app.config.from_object(config_object)
     register_extensions(app)
     register_blueprints(app)
-    # register_errorhandlers(app)
     register_shellcontext(app)
     register_commands(app)
     configure_logger(app)
-
     return app
 
 def register_extensions(app):
@@ -49,58 +40,30 @@ def register_extensions(app):
     bcrypt.init_app(app)
     cache.init_app(app)
     db.init_app(app)
-
     jwtman.init_app(app)
- 
     debug_toolbar.init_app(app)
     migrate.init_app(app, db)
-
     return None
-
 
 def register_blueprints(app):
     """Register Flask blueprints."""
-
     app.register_blueprint(blueprint_edurange3_public)
     app.register_blueprint(blueprint_edurange3_student)
     app.register_blueprint(blueprint_edurange3_instructor)
     app.register_blueprint(blueprint_edurange3_scenarios)
-
     return None
-
-
-# def register_errorhandlers(app):
-#     """Register error handlers."""
-
-#     def render_error(error):
-#         """Render error template."""
-#         error_code = getattr(error, "code", 500)
-#         return (
-#             render_template(f"{error_code}.html"),
-#             error_code,
-#         )
-
-#     for errcode in [401, 403, 404, 500]:
-#         app.errorhandler(errcode)(render_error)
-
-#     return None
-
 
 def register_shellcontext(app):
     """Register shell context objects."""
-
     def shell_context():
         """Shell context objects."""
         return {"db": db, "User": database.models.User} # DEV_CHECK
-
     app.shell_context_processor(shell_context)
-
 
 def register_commands(app):
     """Register Click commands."""
     app.cli.add_command(commands.test)
     app.cli.add_command(commands.lint)
-
 
 def configure_logger(app):
     """Configure loggers."""
