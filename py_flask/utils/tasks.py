fe57c4e090a7ff55631bafb6e52f7096c9469e44
wshine <114693487+WinstonShine@users.noreply.github.com>
2024-02-28 12:40:48 -0800

    fix log collecting to only check the gateway

--- py_flask/utils/tasks.py
+++ py_flask/utils/tasks.py
@@ -361,15 +361,15 @@ def scenarioCollectLogs(self, arg):
         if c_name is not None and c_name != 'ago' and c_name != 'NAMES':
             if c_name.split('_')[0] is not None and c_name.split('_')[0] not in scenarios:
                 scenarios.append(c_name.split('_')[0])
-        try:  # DEV_FIX This is dangerous, may want to substitute for subprocess.call
-            os.system(f'docker cp {c_name}:/usr/local/src/merged_logs.csv logs/{c_name}.csv')
-            os.system(f'docker cp {c_name}:/usr/local/src/raw_logs.zip logs/{c_name}.zip')
-        except FileNotFoundError as e:
-            print(f'{e}')
 
     files = subprocess.run(['ls', 'logs/'], stdout=subprocess.PIPE).stdout.decode('utf-8')
     files = files.split('\n')[:-1]
     for s in scenarios:
+        try:  # DEV_FIX This is dangerous, may want to substitute for subprocess.call
+            os.system(f'docker cp {s}_gateway:/usr/local/src/merged_logs.csv logs/{s}_gateway.csv')
+            os.system(f'docker cp {s}_gateway:/usr/local/src/raw_logs.zip logs/{s}_gateway.zip')
+        except FileNotFoundError as e:
+            print(f'{e}')
         if os.path.isdir(f'scenarios/tmp/{s}'):
             try:
                 os.system(f'cat /dev/null > scenarios/tmp/{s}/{s}-history.csv')
