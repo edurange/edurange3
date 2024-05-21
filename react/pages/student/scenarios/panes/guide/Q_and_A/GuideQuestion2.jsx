fc40d385d01e8730e11db241773ec5253e6ca6b8
exoriparian <jonah777@gmail.com>
2024-05-21 01:10:51 +0000

    duplicate object prop patch - yaml content wip3

--- react/pages/student/scenarios/panes/guide/Q_and_A/GuideQuestion2.jsx
+++ react/pages/student/scenarios/panes/guide/Q_and_A/GuideQuestion2.jsx
@@ -23,13 +23,10 @@ function GuideQuestion2({ scenario_id, questionObj, scenario_type }) {
         }
     };
     
-    console.log('quobby: ', questionObj)
-
     async function handleSubmit() {
         try {
             const evaluated = await axios.post('/check_response', {
                 scenario_id: scenario_id,
-                question_num: questionObj?.itemContentPointer,
                 scenario_type: scenario_type,
                 student_response: inputText_state,
                 question_num: questionObj.question_num
@@ -37,17 +34,21 @@ function GuideQuestion2({ scenario_id, questionObj, scenario_type }) {
             if (evaluated && evaluated.data?.[0]) {
                 const this_item = evaluated.data[0];
                 const new_score = this_item?.points_awarded ?? 0;
-                const current_score = responseData_state[questionObj?.itemContentPointer]?.points_awarded ?? 0;
+                const current_score = responseData_state[questionObj?.question_num]?.points_awarded ?? 0;
 
                 if (new_score > current_score) {
                     set_responseData_state(prevState => ({
                         ...prevState,
-                        [questionObj?.itemContentPointer]: this_item
+                        [questionObj?.question_num]: this_item
                     }));
                 }
 
-                console.log(`Submission for scenario ${scenario_id} question ${questionObj?.itemContentPointer}: "${inputText_state}". Correct answer was [ REDACTED ]. You were awarded ${this_item.points_awarded} points!`);
-                // console.log(`Submission for scenario ${scenario_id} question ${questionObj?.itemContentPointer}: "${inputText_state}". Correct answer was ${this_item.correct_response}. You were awarded ${this_item.points_awarded} points!`);
+                // console.log(`Submission for scenario ${scenario_id} question ${questionObj?.itemContentPointer} was: ${
+                //     inputText_state===this_item.correct_response 
+                //         ? `CORRECT! ✅ \nYou were awarded ${this_item.points_awarded} points!` 
+                //         : "INCORRECT! ❌" 
+                //     }.`);
+                console.log(`Submission for scenario ${scenario_id} question ${questionObj?.itemContentPointer}: "${inputText_state}". Correct answer was ${this_item.correct_response}. You were awarded ${this_item.points_awarded} points!`);
             }
 
             set_inputText_state('');
