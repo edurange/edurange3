adcb9d6173a8b670198879e1e110a8fda673526a
juliascot <juliascott@lclark.edu>
2024-06-28 16:13:25 -0700

    added certificate to the default firefox database

--- install.sh
+++ install.sh
@@ -127,6 +127,9 @@ do
     sudo sed -i "s|listen 80;|listen 443 ssl;\n    ssl_certificate $localCert;\n    ssl_certificate_key $localKey;|g" /etc/nginx/sites-available/default
     cat ./docs/nginx.port80Redirect.snippet | sudo tee -a /etc/nginx/sites-available/default
     sudo sed -i "s/DOMAIN_TO_BE_REPLACED/${localDomain}/g" /etc/nginx/sites-available/default
+    firefoxProfile=$(sudo grep -m 1 -Po '(?<=Path=).*' /$HOME/snap/firefox/common/.mozilla/firefox/profiles.ini)
+    sudo certutil -d sql:/$HOME/snap/firefox/common/.mozilla/firefox/${firefoxProfile} -A -t "C,," -n "EDURange" -i edurange.dev+2.pem
+    
     sudo service nginx reload
     
   elif [ $promptnumber -eq 2 ]; then
