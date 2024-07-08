97a56eac6a2af917b9c2d2c6e5d14cfe2c02c5e2
juliascot <juliascott@lclark.edu>
2024-07-08 11:44:06 -0700

    updated test (including trying commands)

--- user_simulation/initial_test.py
+++ user_simulation/initial_test.py
@@ -3,7 +3,8 @@ from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from profile_info import *
+from selenium.webdriver.common.action_chains import ActionChains
+from profile_info import * # This is a separate document where I set the variables in all-caps
 import time
 
 
@@ -17,7 +18,14 @@ driver = webdriver.Firefox(service=service, options=options)
 username = CREDENTIALS[0][0]
 password = CREDENTIALS[0][1]
 
+def submit_command(command):
+	action = ActionChains(driver)
+	for letter in command:
+		action.key_down(letter).key_up(letter)
+	action.key_down(Keys.RETURN).key_up(Keys.RETURN).perform()
 
+
+# Open EDURange and log in
 driver.maximize_window()
 driver.get("https://edurange.dev")
 time.sleep(1)
@@ -29,7 +37,30 @@ password_field = driver.find_element(By.ID, "password")
 username_field.send_keys(username)
 password_field.send_keys(password)
 password_field.send_keys(Keys.RETURN)
-time.sleep(2)
+time.sleep(1)
+
+# Select specific scenario, open the Web SSH, and click on text area
+most_boxes = driver.find_elements(By.CLASS_NAME, "table-cell-item.highlightable-cell.col-small")
+for box in most_boxes:
+	if box.get_attribute("innerHTML") == SCENARIO_NAME:
+		box.click()
+		break
+time.sleep(1)
+ssh_button = driver.find_element(By.CLASS_NAME, "footcontrol-item.footcontrol-web-ssh-button")
+ssh_button.click()
+time.sleep(1)
+event_box = driver.find_element(By.CLASS_NAME, "terminal.xterm.xterm-dom-renderer-owner-1")
+event_box.click()
+
+# Enter commands
+submit_command("iamfrustrated")
+time.sleep(1)
+submit_command("ls")
+
+
+time.sleep(4)
+
+
 
 
 driver.close()
