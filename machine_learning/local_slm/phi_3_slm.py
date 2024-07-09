27f3c8a698b6c4fc1406ff2a80dce4671092c621
tw-edu <taylor.wolff@evergreen.edu>
2024-07-08 23:05:42 -0700

    Initial commit for phi_3_slm.py

---
+++ machine_learning/local_slm/phi_3_slm.py
@@ -0,0 +1,152 @@
+####################################################################################################
+# PHI-3 SMALL LANGUAGE MODEL MODULE
+# Program for running Microsoft's Phi-3-mini-4k-instruct-q4 small language model,
+# a quantized SLM with only 4b params. Aiming to generate hints based on student bash, chat
+# and answer logs.
+# 
+#
+# Author: Taylor Wolff 
+####################################################################################################
+
+import sys
+import os
+import math
+import pycuda.driver as cuda
+import pycuda.autoinit
+
+import llama_cpp
+from llama_cpp import Llama
+import asyncio
+
+#Checking user hardware specs, this also checks if the user has a local GPU as this allows offloading 
+# and accelerates generation drastically
+def check_hardware_specs():
+
+      def check_cpu_specs():
+            num_cpus = os.cpu_count()
+            if num_cpus == 0 or num_cpus is None:
+                  raise ValueError("No CPU cores detected... how's this possible?")
+            else:
+                  return int(num_cpus)
+            
+      def check_gpu_specs():
+            try:
+                  cuda.init()
+                  num_gpus = cuda.Device.count()
+                  if num_gpus == 0 or num_gpus is None:
+                        return 0
+                  else: 
+                        return num_gpus
+                  
+            except cuda.RuntimeError:
+                  return 0
+
+      num_cpus = check_cpu_specs()
+      num_gpus = check_gpu_specs()
+      
+      return num_cpus, num_gpus
+
+#Calculates scaled core usage, also enables GPU offloading.
+def calculate_hardware_settings(resource_scaler, gpu_enable):
+
+      hardware_specs = check_hardware_specs()
+      num_cpus = hardware_specs[0]
+      num_gpus = hardware_specs[1]
+
+      performance_setting_dict = {
+            "1": 0.25,
+            "2": 0.5,
+            "3": 0.75,
+      }
+
+      set_cpu_resources = math.floor(num_cpus * performance_setting_dict[resource_scaler])
+
+      if num_gpus != 0 and gpu_enable:
+            set_gpu_resources = -1
+      else: 
+            set_gpu_resources = 0
+
+      return set_cpu_resources, set_gpu_resources
+
+
+#Generating the hint.
+async def generate_hint(hardware_settings, question):
+
+      #Echo to user the recources being used.
+      print(f"\nCPU cores being used: {hardware_settings[0]}")
+      print("GPU Enabled\n") if hardware_settings[1] == -1 else print("GPU Disabled\n")
+    
+
+      #The Phi-3 model is quantized and can be found as a .gguf file in the dir.
+      slm = Llama(
+      model_path="machine_learning/local_slm/Phi-3-mini-4k-instruct-q4.gguf", verbose=False,
+      n_ctx=4080, # Uncomment to increase the context window  
+      n_threads=hardware_settings[0], # Threads being used
+      n_gpu_layers=hardware_settings[1]# Uncomment to use GPU acceleration
+      )
+
+      #This can be changed in the future easily, experimenting with prompting.
+      system_prompt = "Provide me a very short hint regarding my bash history: "
+      question_with_system_prompt = f"{system_prompt} {question} "
+      
+      output = slm(
+            f"<|user|>\n{question_with_system_prompt}<|end|>\n<|assistant|>", # Prompt  
+            max_tokens=4080,
+            stop=["<|end|>"], 
+            echo=False, 
+            temperature=0.8,
+      ) 
+
+      return output["choices"][0]["text"]
+
+#This will be the main call function to generate the hint. Eventually it'll take studentID for param for pulling from DB.
+async def get_hint():
+
+      print(" ________________________ ")
+      print("| EDURANGE PHI-3 SLM     | ")
+      print("| ---------------------- | ")
+      print("| ENABLE GPU OFFLOAD?    | ")
+      print("| (Highly recommended)   | ")
+      print("|                        | ")
+      print("| 1. ENABLE              | ")
+      print("| 2. DISABLE             | ")
+      print("|________________________|  ")
+      print("\n")
+
+      #Prompt if GPU should be enabled, if not default to disabled.
+      gpu_enable_setting = int(input())
+      gpu_enable = True if gpu_enable_setting == 1 else False
+
+      
+
+      print(" ________________________ ")
+      print("| EDURANGE PHI-3 SLM     | ")
+      print("| ---------------------- | ")
+      print("| PERFORMANCE SETTING    | ")
+      print("| (MEDIUM is reccomended)| ")
+      print("|                        | ")
+      print("| 1. LOW (.25 of cores)  | ")
+      print("| 2. MEDIUM (.5 of cores)| ")
+      print("| 3. HIGH (.75 of cores) | ")
+      print("|________________________|  ")
+      print("\n")
+
+      #Prompt what performance settings should be used.
+      performance_setting = input()
+
+      #Make function call to calculate exact values.
+      hardware_settings = calculate_hardware_settings(performance_setting, gpu_enable)
+
+      #Prompt for user question
+      print("Enter Bash History Line: ")
+
+      #This will be replaced by a getLogs() call to the db for a given student.
+      question = input()	
+      answer = await generate_hint(hardware_settings, question)
+      print(answer)
+
+def main():
+    asyncio.run(get_hint())
+
+if __name__ == "__main__":
+   main()
