af10593fed4172fbcdea28b89f7014fcb5443078
juliascot <juliascott@lclark.edu>
2024-07-09 13:35:06 -0700

    debugged

--- user_simulation/simulation_setup.sh
+++ user_simulation/simulation_setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # Initial requirements
-pip install selenium
+pip3 install selenium
 wget https://github.com/mozilla/geckodriver/releases/download/v0.34.0/geckodriver-v0.34.0-linux64.tar.gz
 tar xf geckodriver-v0.34.0-linux64.tar.gz
 
 # Making the profile_info.py file
 touch profile_info.py
-firefox_profile = $(sudo grep -m 1 -Po '(?<=Path=).*' /$HOME/snap/firefox/common/.mozilla/firefox/profiles.ini)
-echo 'FIREFOX_PROFILE_PATH = "/home/jsco/snap/firefox/common/.mozilla/firefox/${firefox_profile}"' >> profile_info.py
+firefox_profile=$(sudo grep -m 1 -Po '(?<=Path=).*' /$HOME/snap/firefox/common/.mozilla/firefox/profiles.ini)
+echo 'FIREFOX_PROFILE_PATH = "/home/jsco/snap/firefox/common/.mozilla/firefox/'${firefox_profile}'"' >> profile_info.py
+echo 'CREDENTIALS = ' >> profile_info.py
+echo 'SCENARIO_NAME = ' >> profile_info.py
