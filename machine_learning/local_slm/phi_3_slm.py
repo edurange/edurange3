53e890e3666d313176453e6836044dbc6a8c46e0
Taylor Wolff <taylor.wolff@evergreen.edu>
2024-07-13 14:09:11 -0700

    Storing the prompts into dict which makes string join more efficent

--- machine_learning/local_slm/phi_3_slm.py
+++ machine_learning/local_slm/phi_3_slm.py
@@ -8,17 +8,20 @@
 # Author: Taylor Wolff 
 # Run $python machine_learning/local_slm/phi_3_slm.py to be prompted to input bash commands.
 ####################################################################################################
-import llama_cpp
-from llama_cpp import Llama
 import sys
 import os
 import math
 import pyopencl as cl
-from memory_profiler import profile, memory_usage
-from llama_index.core import Settings
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 import asyncio
 import yaml
+
+import llama_cpp
+from llama_cpp import Llama
+from llama_index.core import Settings
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+
+from memory_profiler import profile, memory_usage
+
 
 #Checking user hardware specs, this also checks if the user has a local GPU as this allows offloading 
 # and accelerates generation drastically
@@ -43,26 +46,28 @@ def check_hardware_specs():
             except Exception as GPU_NOT_FOUND:
                   return 0
       
-            
       num_cpus = get_cpu_specs()
       num_gpus = get_gpu_specs()
       
-      print(num_gpus)
-      print(num_cpus)
-   
-      
       return num_cpus, num_gpus
 
+
+
 #Calculates scaled core usage, also enables GPU offloading.
-def set_cpu_gpu_resources():
+def set_cpu_gpu_resources(gpu_enable, n_threads_scaler):
 
       num_cpus, num_gpus= check_hardware_specs()
 
-      #Logic for setting GPU and CPU resources
-      cpu_resources = num_cpus
-      gpu_resources = 0 if num_gpus == 0 or num_gpus is None else 12
+      #Logic for setting CPU resources
+      cpu_resources = int(num_cpus / n_threads_scaler) if num_cpus >= 4 else ValueError("At least 4 CPU Cores required")
+      gpu_resources = -1 if gpu_enable and num_gpus and num_gpus > 0 else 0
       
+      #Print to CLI the current resources being used
+      print("\nGPU Detected, offloading automatically enabled") if gpu_resources !=0 else print("No GPU detected, program will run on CPU")
+      print(f"Number of threads to be used: {cpu_resources}\n")
       return cpu_resources, gpu_resources
+
+
 
 def prompt_model(slm, prompt):
           
@@ -72,15 +77,17 @@ def prompt_model(slm, prompt):
             max_tokens=4080,
             stop=["<|end|>"], 
             echo=False, 
-            temperature=0.7,
+            temperature=1.0,
       ) 
 
       return output["choices"][0]["text"]
       
 
-def initialize_model():
+
+def initialize_model(gpu_enable, n_threads_scaler):
          
-      cpu_resources, gpu_resources = set_cpu_gpu_resources()
+      cpu_resources, gpu_resources = set_cpu_gpu_resources(gpu_enable, n_threads_scaler)
+      
 
       slm = Llama.from_pretrained(
             repo_id="microsoft/Phi-3-mini-4k-instruct-gguf",
@@ -90,8 +97,10 @@ def initialize_model():
             n_threads=cpu_resources, 
             n_gpu_layers=gpu_resources
       )
-         
+
       return slm
+
+
 
 def retrieve_and_parse_scenario_guide(scenario):
 
@@ -106,25 +115,27 @@ def retrieve_and_parse_scenario_guide(scenario):
 
 def input_context_system(scenario):
 
-      #Additional system and context prompts
-      system_prompt = "You are an AI that assists a struggling student working on a cyber-security scenario, a student interacts with the exercise using the command line terminal. Additionally they may be asked to fill in their answers to questions with forms, you review their recent bash, chat and question answer history and provide them a very short hint relevant to the scenario guide I will provide you now, "
-      scenario_guide_context_preface = " The following is the answers to the questions, do not reveal it's contents to the student. "
-      parsed_scenario_guide = retrieve_and_parse_scenario_guide(scenario) + ". You have now reached the end of the scenario guide. "
-      logs_context_preface = ". Now I will provide you the recent bash, chat and answer history of the student, taking into account the scenario's answers, provide them a hint on what to do next, or how to fix the current errors:  "
-
-      #Current
+      #Will be a function call to getLogs()
       bash_history = "1. $gcc -o empty empty.c 2. gcc: error: empty.c: No such file or directory 3. gcc: fatal error: no input files 4. compilation terminated. 5. $ls 6. $ls"
-      chat_history = "NO MESSAGES RECORDED YET" 
-      answer_history = "NO QUESTIONS ANSWERED YET"
+      chat_history = "1. STUDENT: Why can't I compile?" 
+      answer_history = " "
 
-      bash_history_prompt = "These are the student's recent bash commands: {bash_history}. "
-      chat_history_prompt = "These are the student's recent chat messages: {bash_history}. "
-      answer_history_prompt = "These are the student's recent answers to the question forms: {bash_history}. "
+      prompt_dict = {
+            "system_prompt" : "You are an instruction AI that assists a struggling student working on a cyber-security scenario. You will be provided an answer key to the question their currently completing, as well as the recent bash, chat and answer history of the student. Provide them a hint on what to do next, but prioritize asssisting them with debugging current bash errors they're experiencing. Do not ask them questions",
+            "scenario_guide_context_preface" : " This is the answer do not reveal it's contents directly in any hint. ",
+            "parsed_scenario_guide_end" : ". You the AI have now reached the end of the answer key, you the AI do not reveal it's contents to the student. ",
+            "parsed_scenario_guide": retrieve_and_parse_scenario_guide(scenario),
+            "logs_context_preface" : ". Now I will provide you the recent bash, chat and answer history of the student: ",
+            "bash_history_prompt" : f"These are the student's recent bash commands: {bash_history}. ",
+            "chat_history_prompt" : f"These are the student's recent chat messages: {chat_history}. ",
+            "answer_history_prompt" : f"These are the student's recent answers to the question forms: {answer_history}. "
+      }
+      
 
-      #ending_prompt_reiteration = ". Using the context provided by the scenario guide and the student's recent logs, provide a very short hint considering what part of the scenario they may be at and what they may be struggling with."
-
-      finalized_prompt = system_prompt + scenario_guide_context_preface + parsed_scenario_guide + bash_history_prompt + chat_history_prompt + answer_history_prompt
-
+      #Faster operation to initialize as dict and str.join()
+      finalized_prompt = " ".join(str(value) for value in prompt_dict.values())
+      print("INPUT CONTEXT PROMPT FOR MODEL: ")
+      print(f"\n\n{finalized_prompt}\n")
       return finalized_prompt
 
 #Generating the hint.
@@ -135,13 +146,22 @@ def generate_hint(slm, scenario):
       answer = prompt_model(slm, finalized_prompt)
 
       #Answer
-      print(answer)
+      print("MODEL ANSWER RESPONSE:  ")
+      print(f"\n{answer}")
+
+
+
+
+def run_model_manager(scenario):
+
+      slm = initialize_model(True, 12)
+      
 
 
 #This will be the main call function to generate the hint. Eventually it'll take studentID for param for pulling from DB.
 def main():
     scenario = "strace"
-    slm = initialize_model()
+    slm = initialize_model(True, 12)
     generate_hint(slm, scenario)
 
 if __name__ == "__main__":
